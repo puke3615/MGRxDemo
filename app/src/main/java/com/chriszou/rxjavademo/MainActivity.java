fee630557ad8b216cb25d8f139024f4a1079191f
xiaochuang <xiaochuang@mogujie.com>
2016-03-14 07:41:06 +0800

    username

--- app/src/main/java/com/chriszou/rxjavademo/MainActivity.java
+++ app/src/main/java/com/chriszou/rxjavademo/MainActivity.java
@@ -2,34 +2,34 @@ package com.chriszou.rxjavademo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import rx.Observable;
-import rx.Subscriber;
+import rx.Observer;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Observable<String> observable = Observable.from(new String[]{"hello", "world", "rxjava"});
-        observable.subscribe(new Subscriber<String>() {
+        Observable<String> observable = Observable.from(new String[]{"torvalds", "jakewharton", "chriszou"});
+        observable.subscribe(new Observer<String>() {
             @Override
             public void onCompleted() {
-                Log.d("rxjava-demo", "on completed");
+                System.out.println("on completed");
             }
 
             @Override
             public void onError(Throwable e) {
-
+                System.out.println("on error");
+                e.printStackTrace();
             }
 
             @Override
             public void onNext(String s) {
-
+                System.out.println("on next");
+                System.out.println(s);
             }
         });
-    }
+   }
 }
