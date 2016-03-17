f3030c376d4bc45767f21a34e364d4a1f4ccd978
xiaochuang <xiaochuang@mogujie.com>
2016-03-17 10:37:21 +0800

    Finish operator part

--- app/src/main/java/com/chriszou/rxjavademo/MainActivity.java
+++ app/src/main/java/com/chriszou/rxjavademo/MainActivity.java
@@ -1,21 +1,80 @@
 package com.chriszou.rxjavademo;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.msgView).setOnClickListener(v -> rxjavaTest());
+        rxjavaTest();
+    }
 
-        Observable<String> observable = Observable.from(new String[]{"torvalds", "jakewharton", "chriszou"});
-        observable.subscribe(s -> {
-            System.out.println(s);
-        }, throwable -> {
-            throwable.printStackTrace();
-        });
-   }
+    private void rxjavaTest() {
+        Observable<String> observable = Observable.defer(() -> Observable.from(getSourceItems()));
+        observable.map(s -> "https://api.github.com/users/".concat(s))
+                .toList()
+                .flatMap(strings -> Observable.from(strings))
+                .zipWith(observable, (url, name) -> new Pair<>(name, url))
+                .toList()
+                .flatMap(pairs -> Observable.from(pairs))
+                .map(pair -> getUser(pair.second))
+                .toSortedList((user, user2) -> {
+                    if (user.followerCount == user2.followerCount) return 0;
+                    if (user.followerCount > user2.followerCount) return -1;
+                    return 1;
+                })
+                .flatMap(users -> Observable.from(users))
+                .take(3)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(s -> System.out.println(s), e -> e.printStackTrace()
+                );
+    }
+
+    @NonNull
+    private String[] getSourceItems() {
+        return new String[]{"torvalds", "jakewharton", "dhh", "chriszou", "chrisbanes", "ryanb"};
+    }
+
+    private void printThread(String s) {
+        System.out.println(s + Thread.currentThread().getId());
+    }
+
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    OkHttpClient client = new OkHttpClient();
+
+    private User getUser(String url) {
+        User user = null;
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            user = new Gson().fromJson(response.body().string(), User.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return user;
+    }
 }
---
+++ app/src/main/java/com/chriszou/rxjavademo/User.java
@@ -0,0 +1,34 @@
+package com.chriszou.rxjavademo;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by xiaochuang on 3/16/16.
+ */
+public class User {
+    @SerializedName("login")
+    public final String username;
+    @SerializedName("avatar_url")
+    public final String avartarUrl;
+    @SerializedName("followers")
+    public final int followerCount;
+    @SerializedName("public_repos")
+    public final int repoCount;
+
+    public User(String username, String avartarUrl, int followerCount, int repoCount) {
+        this.username = username;
+        this.avartarUrl = avartarUrl;
+        this.followerCount = followerCount;
+        this.repoCount = repoCount;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", avartarUrl='" + avartarUrl + '\'' +
+                ", followerCount='" + followerCount + '\'' +
+                ", repoCount='" + repoCount + '\'' +
+                '}';
+    }
+}
